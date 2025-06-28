34c2aa075c26a551e5fa3f3e3c17b9701686d1aa
arhahrear30 <arshahrear30@gmail.com>
2025-06-28 23:22:00 +0600

    Create SplashScreen.java

---
+++ SplashScreen.java
@@ -0,0 +1,29 @@
+package com.example.themes;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    //mouse right click>NEW>JAVA Class>Class(Name)
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                startActivity(new Intent(SplashScreen.this,MainActivity.class));
+                finish();
+            }
+        },2000);
+
+
+    }
+}
